064e8441d2a66779015c1e0d742bd2347fa3298d
Adrian Zaragoza <azaragoza.lupian@gmail.com>
2021-04-17 23:50:14 -0700

    Complete User registration

--- app.js
+++ app.js
@@ -2,13 +2,34 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
+
+
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "jim",
+    email: "jim@jim.jim",
+    password: "jimisgreat123"
+  })
+  user.save()
+  res.send("Hello World")
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
-
-
-
-app.get("/", (req, res) => res.send("Hi World"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
